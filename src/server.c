c4aa987986af7aba16e7a6d17b30f5ed17dbf0bd
georgii.imeshkenov <georgiiimeshkenov@gmail.com>
2023-12-23 10:40:00 +0300

    allocating recv_bytes+1 for result

--- src/server.c
+++ src/server.c
@@ -213,7 +213,9 @@ handle_client(int sock_client, char *buff, struct sockaddr *client_addr, socklen
         );
 
         if (recv_bytes > 0) {
-            buff[recv_bytes] = '\0';
+            if (recv_bytes < MAXDATABUFFLEN) {
+                buff[++recv_bytes] = '\0';
+            }
             result = malloc(recv_bytes);
             memcpy(result, buff, recv_bytes);
             printf("%s\n", result);
